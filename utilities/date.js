116e74ea0267e023e6daade38c508b700f1d3137
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-08-04 05:41:53 +0800

    created date format function

---
+++ utilities/date.js
@@ -0,0 +1,15 @@
+const formatDate = (timestamp) => {
+    const date = new Date(timestamp);
+
+    const formatter = new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit'
+    })
+
+    return formatter.format(date);
+}
+
+export default formatDate
