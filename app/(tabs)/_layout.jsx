f62447271af3a493a2684a2d1ba80c2e404959d2
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-08-03 23:46:24 +0800

    modified root layout

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,16 +1,61 @@
-import { Stack } from "expo-router";
+import { DataProvider } from "@/contexts/DataContext";
+import { ThemeProvider, useTheme } from "@/contexts/ThemeContext";
+import { Tabs } from "expo-router";
 
 export default function RootLayout() {
     return (
-        <Stack>
-            <Stack.Screen name = 'index' options={{
+        <ThemeProvider>
+            <DataProvider>
+                <App/>        
+            </DataProvider>
+        </ThemeProvider>
+    )
+}
+
+const App = () => {
+    const { theme } = useTheme();
+
+    return(
+        <Tabs        
+            screenOptions={{
                 headerShown: false,
-                title: 'Home'
-            }}/>
+                tabBarStyle: {
+                    backgroundColor: theme.background,
+                    borderRadius: 16,
+                    marginHorizontal: 16,
+                    marginBottom: 16,
+                    minHeight: 60,
+                    justifyContent: 'center'
+                }                
+            }}
+        >
+            <Tabs.Screen 
+                name = 'index'
+                options = {{
+                    title: 'Home'
+                }}
+            />
+
+            <Tabs.Screen 
+                name = 'order'
+                options = {{
+                    title: 'Order'
+                }}
+            />
+
+            <Tabs.Screen 
+                name = 'transaction'
+                options = {{
+                    title: 'Transaction'
+                }}
+            />
             
-            <Stack.Screen name = 'profile' options={{
-                title: 'Profile'
-            }}/>
-        </Stack>
+            <Tabs.Screen 
+                name = 'profile'
+                options = {{
+                    title: 'Profile'
+                }}
+            />
+        </Tabs>
     )
 }
