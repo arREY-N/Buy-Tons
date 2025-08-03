0e23ab6ba1d62fae87e03fd8714b8b257ef6e828
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-08-03 23:46:46 +0800

    refactored main app layout

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,60 +1,30 @@
-import { router } from 'expo-router';
+import { Header } from '@/components/header';
+import { ItemGallery, Overview } from '@/components/overview';
+import { useTheme } from '@/contexts/ThemeContext';
 import {
-    Pressable,
     StyleSheet,
-    Text,
     View
 } from 'react-native';
 
 const app = () => {
+    const {theme, toggleTheme} = useTheme();
+
+    const styles = StyleSheet.create({  
+        container: {
+            marginHorizontal: '2%',
+            backgroundColor: theme.background,
+            flex: 1,
+            flexDirection: 'column',
+        }  
+    });
+    
     return (
         <View style={styles.container}>
-            <Text style = {styles.text}>Buy Tons</Text>
-            <Text>by Pythons</Text>
-            
-            <Pressable onPress={() => router.push('/profile')}>
-                <Text>Profile</Text>
-            </Pressable>
-            <Pressable onPress={() => router.replace('/landing')}>
-                <Text>Log out</Text>
-            </Pressable>
+            <Header/>
+            <Overview/>       
+            <ItemGallery/>
         </View>
     )
 }
 
 export default app 
-
-const styles = StyleSheet.create({
-    button: {
-        height: 60,
-        borderRadius: 20,
-        backgroundColor: 'rgba(0,0,0,0.75)',
-        padding: 6,
-        textAlign: 'center',
-        justifyContent: 'center'
-    },  
-    buttonText: {
-        margin: 10,
-        color: 'white',
-        padding: 4,
-        fontSize: 16,
-        fontWeight: 'bold'
-    },
-    container: {
-        flex: 1,
-        flexDirection: 'column'
-    },
-    image: {
-        width: '100%',
-        height: '100%',
-        flex: 1,
-        resizeMode: 'cover',
-        justifyContent: 'center'
-    },  
-    text: {
-        color: 'black',
-        fontSize: 42,
-        fontWeight: 'bold',
-        textAlign: 'center'
-    }
-});
