270f3d5861364bc293480ff5a399d9bf12ad92a8
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-08-04 05:43:32 +0800

    added design for order list

--- app/(tabs)/order.jsx
+++ app/(tabs)/order.jsx
@@ -1,16 +1,69 @@
-import Header from '@/components/header'
-import { ItemGallery } from '@/components/overview'
+import { Header } from '@/components/header'
+import useData from '@/contexts/DataContext'
+import formatDate from '@/utilities/date'
+import { router } from 'expo-router'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
 
-const order = () => {
+const styles = StyleSheet.create({
+    order: {
+        flex: 1,
+        borderWidth: 1,
+        borderRadius: 16,
+        margin: 5,
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    paymentInfo: {
+        alignContent: 'right'
+    },
+    customer: {
+        fontWeight: '500'
+    },
+    status: {
+        textAlign: 'right'
+    }
+})
+
+const orderScreen = () => {
     return (
         <View>
             <Header/>
-            <ItemGallery/>
-            <Text>order</Text>
+            <OrderGallery/>
         </View>
     )
 }
 
-export default order
+export const OrderGallery = () => {
+    const { transaction } = useData(); 
+    
+    const renderGalleryTransaction = ({item}) => {
+        return(
+            <Pressable 
+                style = {styles.order} 
+                onPress={() => router.push(`/(order)/${item.id}`)}
+            >
+                <View style = {styles.orderInfo}>
+                    <Text style = {styles.customer}>{item.customer}</Text>
+                    <Text>{formatDate(item.date)}</Text>
+                </View>
+                <View style = {styles.paymentInfo}>
+                    <Text>P {item.amount.toFixed(2)}</Text>
+                    <Text style = {styles.status}>{item.status}</Text>
+                </View>
+            </Pressable>
+        )
+    }
+
+    return(
+        <FlatList
+            data={transaction}
+            renderItem={renderGalleryTransaction}
+            keyExtractor={transaction => transaction.id}
+        /> 
+    )
+}
+
+export default orderScreen
