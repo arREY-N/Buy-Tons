db9f40a3e8c1c7ba739ad48f16fb6deceee95ce0
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-08-04 05:42:36 +0800

    moved data and theme provider to the root layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,23 +1,33 @@
+import { DataProvider } from "@/contexts/DataContext";
+import { ThemeProvider } from "@/contexts/ThemeContext";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
     return (
-        <Stack>
-            <Stack.Screen name = 'landing' options={{
-                headerShown: false,
-                title: 'Landing'
-            }}/>
+        <ThemeProvider>
+            <DataProvider>
+                <Stack>
+                    <Stack.Screen name = 'landing' options={{
+                        headerShown: false,
+                        title: 'Landing'
+                    }}/>
 
-            <Stack.Screen name = '(tabs)' options={{
-                headerShown: false,
-                title: 'Home'
-            }}/>
+                    <Stack.Screen name = '(tabs)' options={{
+                        headerShown: false,
+                        title: 'Home'
+                    }}/>
 
-            <Stack.Screen name = '(auth)' options={{
-                headerShown: false,
-                title: 'Auth'
-            }}/>
+                    <Stack.Screen name = '(auth)' options={{
+                        headerShown: false,
+                        title: 'Auth'
+                    }}/>
 
-        </Stack>
+                    <Stack.Screen name = '(order)/[id]' options={{
+                        headerShown: true,
+                        title: 'Order Details'
+                    }}/>
+                </Stack>
+            </DataProvider>
+        </ThemeProvider>
     )
 }
