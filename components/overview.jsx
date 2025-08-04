46e41ea8c634e52154aeca6c753509c51a67d08f
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-08-04 11:37:37 +0800

    cleaned overview

--- components/overview.jsx
+++ components/overview.jsx
@@ -1,20 +1,20 @@
 import { SubHead } from '@/components/header';
+import Info from '@/components/info';
 import useData from '@/contexts/DataContext';
-import { FlatList, Platform, StyleSheet, Text, View } from "react-native";
-
-const Info = ({title, value}) => {
-    return(
-        <View style = {styles.infoContainer}>
-            <Text style = {styles.infoTitle}>{title}</Text>
-            <Text style = {styles.infoValue}>{value}</Text>
-        </View>
-    )
-}
+import { useTheme } from '@/contexts/ThemeContext';
+import { FlatList, StyleSheet, View } from "react-native";
+import ItemBox from './itemBox';
 
 export const Overview = () => {
     const { totalSales, pending } = useData();
 
-    console.log(pending.length);
+    const styles = StyleSheet.create({
+        overview:{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+        }
+    })
     
     return(
         <View style = {styles.overview}>
@@ -31,19 +31,22 @@ export const Overview = () => {
 
 export const ItemGallery = () => {
     const { data } = useData() 
+    const { theme } = useTheme()
+
+    const styles = StyleSheet.create({
+        empty: {
+            backgroundColor: theme.container,
+            flex: 1,
+            margin: 5
+        }
+    })
 
     const renderGalleryItem = ({item}) => {
         if(item.isSpacer){
             return <View style = {styles.empty}/>
         }
         return(
-            <View style = {styles.item}>
-                <View style={styles.itemImage}/>
-                <View style = {styles.itemInfo}>
-                    <Text>{item.name}</Text>
-                    <Text>P{item.price.toFixed(2)}</Text>
-                </View>
-            </View>
+            <ItemBox item = { item }/>
         )
     }
 
@@ -61,59 +64,3 @@ export const ItemGallery = () => {
 }
 
 export default Overview;
-
-const styles = StyleSheet.create({
-    overview:{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
-    infoContainer: {
-        backgroundColor: '#eee',
-        flex: 1,
-        margin: 5, 
-        padding: 20,
-        borderRadius: 16,
-        ...Platform.select({
-            ios: {
-                shadowColor: '#000',
-                shadowOffset: { width: 0, height: 4},
-                shadowOpacity: 0.2,
-                shadowRadius: 6
-            },
-            android: {
-                elevation: 10,
-            }
-        }),
-    },
-    infoValue: {
-        textAlign: 'right',
-        fontSize: 30,
-        fontWeight: '500'
-    },
-    empty: {
-        flex: 1,
-        margin: 5
-    },
-    item:{
-        borderColor: '#333',
-        borderWidth: 1,
-        borderRadius: 16,
-        flex: 1,
-        alignItems: 'center',
-        margin: 5
-    },
-    itemImage: {
-        backgroundColor: '#ddd',
-        height: 150,
-        width: '100%',
-        borderTopEndRadius: 16,
-        borderTopStartRadius: 16
-    },
-    itemInfo: {
-        width: '90%',
-        margin: 10,
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-})
