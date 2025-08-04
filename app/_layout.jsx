2bd384794b665f39ec5fc01f90fc074f7432491d
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-08-04 11:35:14 +0800

    moved the screens to a separate component

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,33 +1,70 @@
 import { DataProvider } from "@/contexts/DataContext";
-import { ThemeProvider } from "@/contexts/ThemeContext";
+import { ThemeProvider, useTheme } from "@/contexts/ThemeContext";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
     return (
         <ThemeProvider>
             <DataProvider>
-                <Stack>
-                    <Stack.Screen name = 'landing' options={{
-                        headerShown: false,
-                        title: 'Landing'
-                    }}/>
-
-                    <Stack.Screen name = '(tabs)' options={{
-                        headerShown: false,
-                        title: 'Home'
-                    }}/>
-
-                    <Stack.Screen name = '(auth)' options={{
-                        headerShown: false,
-                        title: 'Auth'
-                    }}/>
-
-                    <Stack.Screen name = '(order)/[id]' options={{
-                        headerShown: true,
-                        title: 'Order Details'
-                    }}/>
-                </Stack>
+                <ScreenStack/>
             </DataProvider>
         </ThemeProvider>
+    )
+}
+
+const ScreenStack = () => {
+    const { theme } = useTheme();
+
+    return(
+        <Stack>
+            <Stack.Screen name = 'landing' options={{
+                headerShown: false,
+                title: 'Landing',
+                contentStyle: {
+                    backgroundColor: theme.background
+                }
+            }}/>
+
+            <Stack.Screen name = '(tabs)' options={{
+                headerShown: false,
+                title: 'Home',
+                contentStyle: {
+                    backgroundColor: theme.background
+                }
+            }}/>
+
+            <Stack.Screen name = '(auth)' options={{
+                headerShown: false,
+                title: 'Auth',
+                contentStyle: {
+                    backgroundColor: theme.background
+                }
+            }}/>
+
+            <Stack.Screen name = '(order)/[id]' options={{
+                title: 'Order Details',
+                headerStyle: {
+                    backgroundColor: theme.background
+                },
+                headerShadowVisible: false,
+                headerTintColor: theme.text,
+                contentStyle: {
+                    backgroundColor: theme.background
+                }
+            }}/>
+
+            <Stack.Screen name = '(item)/[id]' options={{
+                title: 'Item Details' ,
+                headerStyle: {
+                    backgroundColor: theme.background
+                },
+                headerShadowVisible: false,
+                headerTintColor: theme.text,
+                contentStyle: {
+                    backgroundColor: theme.background
+                }
+            }}/>
+            
+        </Stack>
     )
 }
