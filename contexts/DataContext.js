00d61d9b88257438550a58c10fd0146b38f68029
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-08-05 12:02:42 +0800

    added payments and revised transaction scheme

--- contexts/DataContext.js
+++ contexts/DataContext.js
@@ -5,6 +5,98 @@ const DataContext = createContext(null);
 export const DataProvider = ({children}) => {
     const [pending, setPending] = useState([])
     const [totalSales, setSales] = useState(1000);
+    const [payments, setPayments] = useState([
+        {
+            id: 1,
+            amount: 400,
+            date: Date.now()
+        },
+        {
+            id: 2,
+            amount: 200,
+            date: new Date(2025, 8, 5, 13, 20, 22)
+        },
+        {
+            id: 3,
+            amount: 100,
+            date: new Date(2025, 8, 5, 13, 30, 22)
+        },
+        {
+            id: 4,
+            amount: 40,
+            date: new Date(2025, 8, 4, 11, 30, 22)
+        },
+        {
+            id: 5,
+            amount: 50,
+            date: new Date(2025, 8, 5, 9, 20, 22)
+        },
+        {
+            id: 6,
+            amount: 100,
+            date: new Date(2025, 8, 5, 14, 20, 22)
+        },
+        {
+            id: 7,
+            amount: 70,
+            date: new Date(2025, 8, 5, 13, 20, 22)
+        }
+    ])
+    const [vouchers, setVouchers] = useState([
+        {
+            id: 1,
+            type: 'PER',
+            value: 0.05
+        },
+        {
+            id: 2,
+            type: 'PER',
+            value: 0.10
+        },
+        {
+            id: 3,
+            type: 'PER',
+            value: 0.15
+        },
+        {
+            id: 4,
+            type: 'AMT',
+            value: 30
+        }
+    ])
+    const [customers, setCustomers] = useState([
+        {
+            id: 1,
+            name: 'Customer A',
+            type: 'TERTIARY',
+            section: 'CS33101',
+            contact: 99999999,
+            facebook: 'Cust A'
+        }, 
+        {
+            id: 2,
+            name: 'Customer B',
+            type: 'SHS',
+            section: 'SHS101',
+            contact: 99999999,
+            facebook: 'Cust B'
+        }, 
+        {
+            id: 3,
+            name: 'Customer C',
+            type: 'FACULTY',
+            contact: 99999999,
+            facebook: 'Cust C'
+        }, 
+        {
+            id: 4,
+            name: 'Customer D',
+            type: 'OUTSIDER',
+            contactPerson: 'Contact Person A',
+            contact: 99999999,
+            facebook: 'Cust D'
+        }, 
+    ]);
     
     const [items, setItems] = useState([
         {id: 1, name: 'Pythons Shirt', price: 300},
@@ -21,43 +113,65 @@ export const DataProvider = ({children}) => {
     const [transaction, setTransaction] = useState([
         {
             id: 1, 
-            customer: 'Customer A', 
-            amount: 300, 
+            customerId: 1, 
+            amount: [
+                {
+                    id: 1,
+                },
+                {
+                    id: 3
+                },
+                {
+                    id: 5
+                }
+            ], 
             date: Date.now(), 
             status: 'PAID',
             items: [
-                { id: 1, item: 'Turing Shirt', quantity: 1}, 
-            ]
+                { id: 1, quantity: 1}, 
+            ],
+            voucherId: 3
         },
         {
             id: 2, 
-            customer: 'Customer B', 
-            amount: 350, 
+            customerId: 2, 
+            amount: [
+                {
+                    id: 2,
+                },
+                {
+                    id: 4
+                },
+            ],  
             date: Date.now(), 
             status: 'DP',
             items: [
-                { id: 1, item: 'Ada Shirt', quantity: 2}
-            ]
+                { id: 1, quantity: 2}
+            ],
+            voucherId: 1
         },
         {
             id: 3, 
-            customer: 'Customer C', 
-            amount: 650, 
+            customerId: 3, 
+            amount: [
+                {
+                    id: 1,
+                }
+            ], 
             date: Date.now(), 
             status: 'CLAIMED',
             items: [
-                { id: 1, item: 'Turing Shirt', quantity: 1},
-                { id: 2, item: 'Ada Shirt', quantity: 1}
+                { id: 1, quantity: 1},
+                { id: 2, quantity: 1}
             ]
         },
         {
             id: 4, 
-            customer: 'Customer D', 
-            amount: 78, 
+            customerId: 4,
             date: Date.now(), 
             status: 'READY',
             items: [
-                { id: 1, item: 'Sticker Set', quantity : 2}
+                { id: 3, quantity : 2}
             ]
         },
     ]);
@@ -82,6 +196,12 @@ export const DataProvider = ({children}) => {
             transaction,
             pending,
             totalSales,
+            customers,
+            vouchers,
+            payments, 
+            setPayments,
+            setVouchers,
+            setCustomers,
             setSales,
             setItems,
             setPending,
