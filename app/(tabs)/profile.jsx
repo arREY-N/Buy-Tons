f7975cc7980e303a31c3b678d83702b23fc1b02f
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-08-06 14:31:50 +0800

    added container style

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import StyledText from '@/components/styledText';
+import { useTheme } from '@/contexts/ThemeContext';
+import React from 'react';
+import { View } from 'react-native';
 
 const profile = () => {
+    const { ContainerStyle } = useTheme();
+
     return (
-        <View>
-            <Text>profile</Text>
+        <View style = {ContainerStyle.container}>
+            <StyledText>profile</StyledText>
         </View>
     )
 }
