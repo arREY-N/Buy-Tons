4d572c5c0dee864dd13b10b3f9a00aad9e38553f
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-08-06 14:30:43 +0800

    revised style nesting

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,57 +1,52 @@
 import { useTheme } from "@/contexts/ThemeContext";
 import { Tabs } from "expo-router";
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 
 export default function RootLayout() {
+    const { ContainerStyle } = useTheme();
+    
     return (
-        <App/>
+        <View style = {ContainerStyle.container}>
+            <App/>
+        </View>
     )
 }
 
 const App = () => {
     const { theme } = useTheme();
-
-    const styles = StyleSheet.create({
-        container: {
-            backgroundColor: theme.background,
-            flex: 1
-        }
-    })
-
+    
     return(
-        <View style = {styles.container}>
-            <Tabs 
-                screenOptions={{
-                    headerShown: false,
-                    tabBarStyle: {
-                        backgroundColor: theme.container,
-                        borderColor: theme.container,
-                        height: 80,
-                        justifyContent: 'center',
-                    }                
+        <Tabs 
+            screenOptions={{
+                headerShown: false,
+                tabBarStyle: {
+                    backgroundColor: theme.container,
+                    borderColor: theme.container,
+                    height: 80,
+                    justifyContent: 'center',
+                }                
+            }}
+        >
+            <Tabs.Screen 
+                name = 'index'
+                options = {{
+                    title: 'Home',
                 }}
-            >
-                <Tabs.Screen 
-                    name = 'index'
-                    options = {{
-                        title: 'Home',
-                    }}
-                />
+            />
 
-                <Tabs.Screen 
-                    name = 'order'
-                    options = {{
-                        title: 'Order'
-                    }}
-                />
-                
-                <Tabs.Screen 
-                    name = 'profile'
-                    options = {{
-                        title: 'Profile'
-                    }}
-                />
-            </Tabs>
-        </View>
+            <Tabs.Screen 
+                name = 'order'
+                options = {{
+                    title: 'Order'
+                }}
+            />
+            
+            <Tabs.Screen 
+                name = 'profile'
+                options = {{
+                    title: 'Profile'
+                }}
+            />
+        </Tabs>
     )
 }
