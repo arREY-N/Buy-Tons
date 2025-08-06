f9103f5ef9e2edf16e020a89ab8d3467a5753b85
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-08-06 14:31:31 +0800

    change background and text styles

--- app/(tabs)/order.jsx
+++ app/(tabs)/order.jsx
@@ -1,35 +1,18 @@
 import { Header } from '@/components/header'
+import StyledText from '@/components/styledText'
+import globals from '@/constants/globals'
 import useData from '@/contexts/DataContext'
+import { useTheme } from '@/contexts/ThemeContext'
 import formatDate from '@/utilities/date'
 import { router } from 'expo-router'
 import React from 'react'
-import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native'
-
-const styles = StyleSheet.create({
-    order: {
-        flex: 1,
-        borderWidth: 1,
-        borderRadius: 16,
-        margin: 5,
-        padding: 10,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    },
-    paymentInfo: {
-        alignItems: 'flex-end'
-    },
-    customer: {
-        fontWeight: '500'
-    },
-    status: {
-        textAlign: 'right'
-    }
-})
+import { FlatList, Pressable, StyleSheet, View } from 'react-native'
 
 const OrderScreen = () => {
+    const { ContainerStyle } = useTheme();
+
     return (
-        <View>
+        <View style = {ContainerStyle.container}>
             <Header/>
             <OrderGallery/>
         </View>
@@ -38,7 +21,28 @@ const OrderScreen = () => {
 
 export const OrderGallery = () => {
     const { transaction, getPaymentInfo, getOrderInfo, customers } = useData(); 
-    
+    const { theme } = useTheme();
+
+    const styles = StyleSheet.create({
+        order: {
+            flex: 1,
+            justifyContent: 'space-between',
+            backgroundColor: theme.container,
+            flexDirection: 'row',
+            marginVertical: 5,
+            marginHorizontal: 10,
+            padding: 15,
+            borderRadius: globals.radius,
+            alignItems: 'center'
+        },
+        paymentInfo: {
+            alignItems: 'flex-end'
+        },
+        customer: {
+            fontWeight: '500'
+        }
+    })
+
     const renderGalleryTransaction = ({item}) => {
         const { totalPayment } = item.amount !== undefined ? 
             getPaymentInfo(item.amount) : getPaymentInfo([]);
@@ -52,12 +56,12 @@ export const OrderGallery = () => {
                 onPress={() => router.push(`/(order)/${item.id}`)}
             >
                 <View style = {styles.orderInfo}>
-                    <Text style = {styles.customer}>{customer.name}</Text>
-                    <Text>{formatDate(item.date)}</Text>
+                    <StyledText style = {styles.customer}>{customer.name}</StyledText>
+                    <StyledText>{formatDate(item.date)}</StyledText>
                 </View>
                 <View style = {styles.paymentInfo}>
-                    <Text>P {amountToPay.toFixed(2)}</Text>
-                    <Text>P {totalPayment.toFixed(2)}</Text>
+                    <StyledText>P {amountToPay.toFixed(2)} / {totalPayment.toFixed(2)}</StyledText>
+                    <StyledText>{item.status}</StyledText>
                 </View>
             </Pressable>
         )
