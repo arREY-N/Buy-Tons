5fac57faceb3a19a9295c3892799bca3f63a05e6
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-08-08 12:06:11 +0800

    modified input box styling

--- components/input.jsx
+++ components/input.jsx
@@ -1,8 +1,30 @@
+import globals from "@/constants/globals";
+import { useTheme } from "@/contexts/ThemeContext";
 import { useState } from "react";
 import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export const CustomTextInput = ({ label, placeholder, value, onChangeText, secureTextEntry = false }) => {
+    const { theme } = useTheme();
+
     const [isFocused, setIsFocused] = useState(false);
+    const style = StyleSheet.create({
+        input: {
+            borderWidth: 1,
+            padding: 10,
+            borderRadius: globals.radius,
+            color: theme.text,
+            borderColor: theme.text,
+            placeholderTextColor: theme.placeholder,
+            minWidth: '300px'
+        },
+        inputFocused: {
+            borderColor: '#a22121ff'
+        },
+        inputLabel: {
+            margin: 5,
+            color: theme.text
+        }
+    })
     
     return (
         <View>
@@ -23,19 +45,3 @@ export const CustomTextInput = ({ label, placeholder, value, onChangeText, secur
 
 export default CustomTextInput
 
-const style = StyleSheet.create({
-    input: {
-        borderWidth: 1,
-        padding: 10,
-        borderRadius: 15,
-        borderColor: '#292929ff',
-        placeholderTextColor: '#3333335c',
-        minWidth: '300px'
-    },
-    inputFocused: {
-        borderColor: '#a22121ff'
-    },
-    inputLabel: {
-        margin: 5
-    }
-})
