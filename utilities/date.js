a6ff962ce8ada02b6ab3bb84a3d2a9ff89918c8d
Reyn Penus <159505551+arREY-N@users.noreply.github.com>
2025-08-08 12:03:57 +0800

    added a date only formatter

--- utilities/date.js
+++ utilities/date.js
@@ -1,4 +1,4 @@
-const formatDate = (timestamp) => {
+export const formatDate = (timestamp) => {
     const date = new Date(timestamp);
 
     const formatter = new Intl.DateTimeFormat('en-US', {
@@ -7,6 +7,18 @@ const formatDate = (timestamp) => {
         day: '2-digit',
         hour: '2-digit',
         minute: '2-digit'
+    })
+
+    return formatter.format(date);
+}
+
+export const formatDateOnly = (timestamp) => {
+    const date = new Date(timestamp);
+
+    const formatter = new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: '2-digit',
     })
 
     return formatter.format(date);
